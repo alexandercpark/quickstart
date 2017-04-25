7e2852802facac35c3285c4f08132fa597db6803
Alexander Park <alexandercpark@gmail.com>
2017-04-25 18:00:35 -0400

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -15,25 +17,18 @@ const HEROES: Hero[] = [
 
 @Component({
   selector: 'my-app',
-  template: `
-	<h2>My Heroes</h2>
-		<ul class="heroes">
-			<li *ngFor="let hero of heroes"
-			  [class.selected]="hero === selectedHero"
-			  (click)="onSelect(hero)">
-			  <span class="badge">{{hero.id}}</span> {{hero.name}}
-			</li>
-		</ul>
-  	<h1>{{title}}</h1>
-  	<div *ngIf="selectedHero">
-		<h2>{{selectedHero.name}} details!</h2>
-		<div><label>id: </label>{{selectedHero.id}}</div>
-		<div>
-		    <label>name: </label>
-		    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-		</div>
-	</div>
-  `,
+	template: `
+	  <h1>{{title}}</h1>
+	  <h2>My Heroes</h2>
+	  <ul class="heroes">
+	    <li *ngFor="let hero of heroes"
+	      [class.selected]="hero === selectedHero"
+	      (click)="onSelect(hero)">
+	      <span class="badge">{{hero.id}}</span> {{hero.name}}
+	    </li>
+	  </ul>
+	  <hero-detail [hero]="selectedHero"></hero-detail>
+	`,
   styles: [`
   .selected {
     background-color: #CFD8DC !important;
@@ -87,6 +82,7 @@ const HEROES: Hero[] = [
 
 
 export class AppComponent  {
+	hero: Hero;
 	title = 'Tour of Heroes';
 	heroes = HEROES;
 	selectedHero: Hero;
@@ -94,9 +90,4 @@ export class AppComponent  {
 	onSelect(hero: Hero): void {
   		this.selectedHero = hero;
 	}
-}
-
-export class Hero {
-	id: number;
-	name: string;
 }
